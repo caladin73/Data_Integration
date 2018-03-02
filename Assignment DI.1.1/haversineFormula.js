852fcf5fb5858402ce239148723d0c53e6148d05
Peter <myrvig@gmail.com>
2018-03-02 07:26:03 +0100

    no message

--- Assignment DI.1.1/haversineFormula.js
+++ Assignment DI.1.1/haversineFormula.js
@@ -1,3 +1,4 @@
+//function getDistanceFromLatLonInKm(61.6487504,30.5926449,13.45529,43.94704) {
 function getDistanceFromLatLonInKm(lat1,lon1,lat2,lon2) {
   var R = 6371; // Radius of the earth in km
   var dLat = deg2rad(lat2-lat1);  // deg2rad below
