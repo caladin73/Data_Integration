202d87426a70b904ffd99a1c9de9ff952f4d42c5
Peter <myrvig@gmail.com>
2018-03-04 17:16:22 +0100

    op

--- DI.2.1/action.php
+++ DI.2.1/action.php
@@ -8,25 +8,17 @@ $sql = "select country.capital as country_code, country.name as Country, country
 $sql .= " from country";
 $sql .= " inner join city on country.capital = city.id";
 $sql .= " where country.name =  '" . $_GET["country"] . "'";
-
 $res = $dbh->query($sql);
 
 $a = array();
-
 foreach ($res as $out) {
     array_push($a, $out);
 }
 
 $arrlength = count($a);
-
-
 for($x = 0; $x < $arrlength; $x++) {
 }
 
-$xmit = json_encode($a);
-
+$xmit = json_encode($out);
 header('Content-Type: application/json');
 print($xmit);
-
-
-
