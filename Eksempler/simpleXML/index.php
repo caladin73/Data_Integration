723290997597793334fc2eecbe211534fdfb6ade
Peter Myrvig <myrvig@gmail.com>
2018-03-05 11:39:38 +0100

    op

---
+++ Eksempler/simpleXML/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title></title>
+</head>
+<body>
+<a href='./formCars.php'>Insert Car</a>
+<a href='./readCars.php'>List Cars</a>
+</body>
+</html>
