febcbb8980a862ea7c22e8f18af46ed3e7f79a9e
Peter <myrvig@gmail.com>
2018-03-06 12:21:10 +0100

    op

--- DI.3.0/insertBook.php
+++ DI.3.0/insertBook.php
@@ -16,11 +16,24 @@ $id++;                                  // increment highest id for use
 $book = $xml_books->addChild('book');
 $book->addAttribute('ref', $ref);
 $book->addAttribute('mdu', $mdu);
-$book->addChild('title', $title);
-$book->addChild('edition', $edition);
-$book->addChild('firstname', $firstname);
-$dealersecurity  = $car->addChild('dealersecurity');
-$dealersecurity->addAttribute('buyback', $buyback);
+
+$authors = $book->addChild('authors');
+$author = $authors->addChild('author');
+$author->addChild('firstname', $firstname);
+$author->addChild('lastname', $lastname);
+
+$publisher = $book->addChild('publisher');
+$publisher->addChild('name', $publisher_name);
+$publisher->addChild('year', $publisher_year);
+$publisher->addChild('place', $publisher_place);
+
+$book->addChild('pages', $pages);
+$book->addChild('isbn', $isbn);
+$book->addChild('price', $price);
+$book->addChild('currency', $currency);
+
+$comments = $book->addChild('comments');
+$comments->addChild('comment', $comment);
 
 $book->asXML($file);
 
