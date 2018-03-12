7d6255afa230cabac8b4519f159b75730a2723c2
Peter <myrvig@gmail.com>
2018-03-12 20:46:52 +0100

    op

--- DI.4.1/select.php
+++ DI.4.1/select.php
@@ -6,9 +6,9 @@ $dbh = DbH::getDbH();
 $q = $dbh->prepare("select * from city");
 $q->execute();
 
-
-//jeg fandt ud af at jeg er nød til at encode array om, fordi der er tegn i data som json_encode ikke godtager
-//så det gør jeg på den her måde, ved at bruge mb_convert_encoding før jeg gemmer datane i array
+//jeg fandt ud af at jeg er nød til at encode array før json_encode, eller kommer der en fejl pga mærkelig tegn:
+// "malformed utf-8 characters possibly incorrectly encoded"
+//ved at bruge mb_convert_encoding før jeg gemmer datane i array, kan jeg løse det
 while($row = $q->fetch(PDO::FETCH_ASSOC))
 {
     foreach($row as &$value)
@@ -20,9 +20,6 @@ while($row = $q->fetch(PDO::FETCH_ASSOC))
 }
 
 $json = json_encode($result);
-var_dump($json);
-die();
-
 
 if ($json)
     file_put_contents('result.json', $json);
