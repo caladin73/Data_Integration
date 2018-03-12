1ac18e06739262d4d059aae3f67add38c3feedd6
Peter <myrvig@gmail.com>
2018-03-12 20:40:12 +0100

    op

--- DI.4.1/select.php
+++ DI.4.1/select.php
@@ -3,28 +3,31 @@ require_once 'inc/DbP.inc.php';
 require_once 'inc/DbH.inc.php';
 
 $dbh = DbH::getDbH();
+$q = $dbh->prepare("select * from city");
+$q->execute();
 
 
-class city {
-    public $id;
-    public $name;
-    public $countrycode;
-    public $district;
-    public $population;
+//jeg fandt ud af at jeg er nød til at encode array om, fordi der er tegn i data som json_encode ikke godtager
+//så det gør jeg på den her måde, ved at bruge mb_convert_encoding før jeg gemmer datane i array
+while($row = $q->fetch(PDO::FETCH_ASSOC))
+{
+    foreach($row as &$value)
+    {
+        $value = mb_convert_encoding($value, "UTF-8", "Windows-1252");
+    }
+    unset($value); # safety: remove reference
+    $result[] = array_map('utf8_encode', $row );
 }
 
-$sth = $dbh->prepare("select * from city limit 5");
-$sth->execute();
-
-$result = $sth->fetchAll(PDO::FETCH_CLASS, "city");
-//var_dump($result);
-
-$content = json_encode($result);
-file_put_contents('result.json', $content);
+$json = json_encode($result);
+var_dump($json);
+die();
 
 
-
-
+if ($json)
+    file_put_contents('result.json', $json);
+else
+    echo json_last_error_msg();
 
 
 
