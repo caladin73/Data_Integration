d727d60e414bb7e2fd60761b66ce358ff7f67c05
Peter <myrvig@gmail.com>
2018-03-16 20:28:14 +0100

    op

--- DI.6.1/data.php
+++ DI.6.1/data.php
@@ -6,8 +6,7 @@ if ($_GET['city']) {
     $city = $_GET['zip'];
 }
 $country = $_GET['country'];
-//$url = "http://api.openweathermap.org/data/2.5/weather?q=" . $city . "," . $country . "&appid=07e02ba24a3caf7f46c40b512f315fc6";
-$url = "http://api.openweathermap.org/data/2.5/weather?q=Kolding,dk&appid=07e02ba24a3caf7f46c40b512f315fc6";
+$url = "http://api.openweathermap.org/data/2.5/weather?q=" . $city . "," . $country . "&appid=07e02ba24a3caf7f46c40b512f315fc6";
 echo $url . "<br>";
 
 $chandle = curl_init($url);
@@ -29,7 +28,10 @@ $obj = json_decode($res); // unserialize to json obj
     <?php
     $tab = "<table>
             <tr>
-                <th colspan='2'>Current in %s</th>
+                <th colspan='2'>%s</th>
+            </tr>
+            <tr>
+                <th colspan='2'>%s</th>
             </tr>
             <tr>
                 <td>Temp [C]</td><td>%s</td>
@@ -41,29 +43,59 @@ $obj = json_decode($res); // unserialize to json obj
                 <td>Wind direction [deg].</td><td>%s</td>
             </tr>
         </table>";
-    printf($tab."\n", $obj->name, round(K2C($obj->main->temp), 0),
+
+    printf($tab."\n", $obj->name, strftime("%A", $obj->dt), round(K2C($obj->main->temp), 0),
+        $obj->main->pressure,
+        $obj->wind->deg);
+    //api.openweathermap.org/data/2.5/forecast?zip=6000,dk
+    //http://samples.openweathermap.org/data/2.5/forecast?id=524901&appid=b6907d289e10d714a6e88b30761fae22
+$url5 = "http://api.openweathermap.org/data/2.5/forecast?q=" . $city . "," . $country . "&appid=07e02ba24a3caf7f46c40b512f315fc6";
+echo $url5 . "<br>";
+    $chandle = curl_init($url5);
+    curl_setopt($chandle, CURLOPT_RETURNTRANSFER, true);
+    $res = curl_exec($chandle);
+    // receiced as a one line json string
+    $obj = json_decode($res); // unserialize to json obj
+    //prettyPrintForDebug($obj);   // comment out after debugging
+
+?>
+
+
+<table>
+            <tr>
+                <th colspan='2'><?php printf($obj->name); ?></th>
+            </tr>
+<?php
+    $five_days = "
+            <tr>
+                <th colspan='2'>%s</th>
+            </tr>
+            <tr>
+                <td>Temp [C]</td><td>%s</td>
+            </tr>
+            <tr>
+                <td>Pressure [hPa]</td><td>%s</td>
+            </tr>
+            <tr>
+                <td>Wind direction [deg].</td><td>%s</td>
+            </tr>
+        </table>";
+
+
+
+    printf($five_days."\n", strftime("%A", $obj->dt), round(K2C($obj->main->temp), 0),
         $obj->main->pressure,
         $obj->wind->deg);
 
-    $tab1 = "<table>
-            <tr>
-                <th colspan='2'>5 days forecast for: %s</th>
-            </tr>
-            <tr>
-                <td>Temp [C]</td><td>%s</td>
-            </tr>
-            <tr>
-                <td>Pressure [hPa]</td><td>%s</td>
-            </tr>
-            <tr>
-                <td>Wind direction [deg].</td><td>%s</td>
-            </tr>
-        </table>";
-    printf($tab1."\n", $obj->name, round(K2C($obj->main->temp), 0),
-        $obj->main->pressure,
-        $obj->wind->deg);
+
+
+
+
+
+
+
+
     ?>
-
 
     </body>
 
