0ad772ee61595e7449237f8391b502f4ab087adb
Peter Myrvig <myrvig@gmail.com>
2018-03-16 10:33:15 +0100

    op

--- DI.6.1/country_abbreviation.php
+++ DI.6.1/country_abbreviation.php
@@ -9,6 +9,22 @@
 $url = 'https://pkgstore.datahub.io/core/country-list/data_json/data/8c458f2d15d9f2119654b29ede6e45b8/data_json.json';
 $json = file_get_contents($url);
 $content = json_decode($json, true);
+?>
 
-foreach($content as $i => $item) {}
+    <table border="1">
+        <tr>
+            <th>Number</th>
+            <th>Code</th>
+            <th>Name</th>
+        </tr>
 
+<?php
+foreach($content as $i => $item) {
+    echo "<tr>";
+    echo "<td>" . ($i*1+1) . "</td>";
+    echo "<td>" . $content[$i]['Code'] . "</td>";
+    echo "<td>" . $content[$i]['Name'] . "</td>";
+    echo "</tr>";
+}
+?>
+    </table>
