2e08f13ddb17c7f904608d3804b8e11ae31dc471
Peter <myrvig@gmail.com>
2018-03-26 10:25:15 +0200

    final 6.1

--- DI.6.1/data.php
+++ DI.6.1/data.php
@@ -47,10 +47,9 @@ $obj = json_decode($res); // unserialize to json obj
     printf($tab."\n", $obj->name, strftime("%A", $obj->dt), round(K2C($obj->main->temp), 0),
         $obj->main->pressure,
         $obj->wind->deg);
-    //api.openweathermap.org/data/2.5/forecast?zip=6000,dk
-    //http://samples.openweathermap.org/data/2.5/forecast?id=524901&appid=b6907d289e10d714a6e88b30761fae22
+
 $url5 = "http://api.openweathermap.org/data/2.5/forecast?q=" . $city . "," . $country . "&appid=07e02ba24a3caf7f46c40b512f315fc6";
-echo $url5 . "<br>";
+
     $chandle = curl_init($url5);
     curl_setopt($chandle, CURLOPT_RETURNTRANSFER, true);
     $res = curl_exec($chandle);
@@ -61,50 +60,9 @@ echo $url5 . "<br>";
 ?>
 
 
-<table>
-            <tr>
-                <th colspan='2'><?php printf($obj->name); ?></th>
-            </tr>
-<?php
-    $five_days = "
-            <tr>
-                <th colspan='2'>%s</th>
-            </tr>
-            <tr>
-                <td>Temp [C]</td><td>%s</td>
-            </tr>
-            <tr>
-                <td>Pressure [hPa]</td><td>%s</td>
-            </tr>
-            <tr>
-                <td>Wind direction [deg].</td><td>%s</td>
-            </tr>
-        </table>";
+</body>
 
-
-
-    printf($five_days."\n", strftime("%A", $obj->dt), round(K2C($obj->main->temp), 0),
-        $obj->main->pressure,
-        $obj->wind->deg);
-
-
-
-
-
-
-
-
-
-    ?>
-
-    </body>
-
-
-
-
-
-
-    </html>
+</html>
 <?php
 
 /*
